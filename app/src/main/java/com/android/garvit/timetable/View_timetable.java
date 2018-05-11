8d1532dcbf4c64c9f6c2ef01ba255bfeaa162078
Daiivol <daiivol.person@gmail.com>
2018-05-11 15:23:32 +0400

    Third commit
    Finally completed timetable layout (without colors)
    added way to set text in table without typing each textfield individually in activity (use for loop)

--- app/src/main/java/com/android/garvit/timetable/View_timetable.java
+++ app/src/main/java/com/android/garvit/timetable/View_timetable.java
@@ -1,13 +1,18 @@
 package com.android.garvit.timetable;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class View_timetable extends AppCompatActivity {
-
+//    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_timetable);
+//        context= this.context;
+//        int resID = context.getResources().getIdentifier(String, "layout", context.getPackageName());
+        String S = new String("activity_view_timetable");
+        int resID = getResources().getIdentifier(S, "layout", getPackageName());
+        setContentView(resID);
     }
 }
